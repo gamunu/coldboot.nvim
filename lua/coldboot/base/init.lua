41fc15e9936bac8fb046283b198c0897243380f3
Gamunu Balagalla <gamunu@fastcode.io>
2024-04-16 21:28:00 +0530

    fix: prioratize yamlfmt

--- lua/coldboot/base/init.lua
+++ lua/coldboot/base/init.lua
@@ -1,204 +1,258 @@
 -- NOTE  : this is where the neovim plugins are defined
 -- if you are looking for vscode specific plugins look for the coldboot/vscode
 -- directory
-return { -- Git related plugins
-'tpope/vim-fugitive', 'tpope/vim-rhubarb', -- Detect tabstop and shiftwidth automatically
-'tpope/vim-sleuth', -- NOTE: This is where your plugins related to LSP can be installed.
---  The configuration is done below. Search for lspconfig to find it below.
-{
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = { -- Automatically install LSPs to stdpath for neovim
+return {                                       -- Git related plugins
+    'tpope/vim-fugitive', 'tpope/vim-rhubarb', -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',                        -- NOTE: This is where your plugins related to LSP can be installed.
+    --  The configuration is done below. Search for lspconfig to find it below.
     {
-        'williamboman/mason.nvim',
-        config = true
-    }, 'williamboman/mason-lspconfig.nvim', -- Useful status updates for LSP
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+        -- LSP Configuration & Plugins
+        'neovim/nvim-lspconfig',
+        dependencies = { -- Automatically install LSPs to stdpath for neovim
+            {
+                'williamboman/mason.nvim',
+                config = true
+            }, 'williamboman/mason-lspconfig.nvim', -- Useful status updates for LSP
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            {
+                'j-hui/fidget.nvim',
+                tag = 'legacy',
+                opts = {}
+            }, -- Additional lua configuration, makes nvim stuff amazing!
+            'folke/neodev.nvim' }
+    },
     {
-        'j-hui/fidget.nvim',
-        tag = 'legacy',
-        opts = {}
-    }, -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim'}
-}, {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = { -- Snippet Engine & its associated nvim-cmp source
-    'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', -- Adds LSP completion capabilities
-    'hrsh7th/cmp-nvim-lsp', -- Add LSP completion path
-    'hrsh7th/cmp-path', -- Adds a number of user-friendly snippets
-    'rafamadriz/friendly-snippets'}
-}, {
-    'stevearc/conform.nvim',
-    event = {'BufWritePre'},
-    cmd = 'ConformInfo',
-    keys = {{
-        -- Customize or remove this keymap to your liking
-        "<leader>f",
-        function()
-            require("conform").format({
-                async = true,
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {                                    -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', -- Adds LSP completion capabilities
+            'hrsh7th/cmp-nvim-lsp',                         -- Add LSP completion path
+            'hrsh7th/cmp-path',                             -- Adds a number of user-friendly snippets
+            'rafamadriz/friendly-snippets' },
+        config = function()
+            -- See `:help cmp`
+            local cmp = require 'cmp'
+            local luasnip = require 'luasnip'
+            luasnip.config.setup {}
+
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                completion = { completeopt = 'menu,menuone,noinsert' },
+
+                -- For an understanding of why these mappings were
+                -- chosen, you will need to read `:help ins-completion`
+                --
+                -- No, but seriously. Please read `:help ins-completion`, it is really good!
+                mapping = cmp.mapping.preset.insert {
+                    -- Select the [n]ext item
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    -- Select the [p]revious item
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+                    -- Scroll the documentation window [b]ack / [f]orward
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+                    -- Accept ([y]es) the completion.
+                    --  This will auto-import if your LSP supports it.
+                    --  This will expand snippets if the LSP sent a snippet.
+                    ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+                    -- Manually trigger a completion from nvim-cmp.
+                    --  Generally you don't need this, because nvim-cmp will display
+                    --  completions whenever it has completion options available.
+                    ['<C-Space>'] = cmp.mapping.complete {},
+
+                    -- Think of <c-l> as moving to the right of your snippet expansion.
+                    --  So if you have a snippet that's like:
+                    --  function $name($args)
+                    --    $body
+                    --  end
+                    --
+                    -- <c-l> will move you to the right of each of the expansion locations.
+                    -- <c-h> is similar, except moving you backwards.
+                    ['<C-l>'] = cmp.mapping(function()
+                        if luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-h>'] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { 'i', 's' }),
+
+                    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+                    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+                }
+
+            }
+        end
+    },
+    {
+        'stevearc/conform.nvim',
+        event = { 'BufWritePre' },
+        cmd = 'ConformInfo',
+        keys = {
+            {
+                -- Customize or remove this keymap to your liking
+                "<leader>f",
+                function()
+                    require("conform").format({
+                        async = true,
+                        lsp_fallback = true
+                    })
+                end,
+                mode = "",
+                desc = "Format buffer"
+            }
+        },
+        -- Everything in opts will be passed to setup()
+        opts = {
+            -- Define your formatters
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "isort", "black" },
+                javascript = { { "eslint_d" } },
+                yaml = { "yamlfmt", "yamlfix" }
+            },
+            -- Set up format-on-save
+            format_on_save = {
+                timeout_ms = 500,
                 lsp_fallback = true
-            })
-        end,
-        mode = "",
-        desc = "Format buffer"
-    }},
-    -- Everything in opts will be passed to setup()
-    opts = {
-        -- Define your formatters
-        formatters_by_ft = {
-            lua = {"stylua"},
-            python = {"isort", "black"},
-            javascript = {{"eslint_d"}},
-            yaml = {"yamlfix", "yamlfmt"}
+            },
+            -- Customize formatters
+            formatters = {
+                shfmt = {
+                    prepend_args = { "-i", "2" }
+                }
+            }
         },
-        -- Set up format-on-save
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true
-        },
-        -- Customize formatters
-        formatters = {
-            shfmt = {
-                prepend_args = {"-i", "2"}
+        init = function()
+            -- If you want the formatexpr, here is the place to set it
+            vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+        end
+    }, -- Useful plugin to show you pending keybinds.
+    {
+        'folke/which-key.nvim',
+        opts = {}
+    },
+    {
+        -- Adds git related signs to the gutter, as well as utilities for managing changes
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            -- See `:help gitsigns.txt`
+            signs = {
+                add = {
+                    text = '+'
+                },
+                change = {
+                    text = '~'
+                },
+                delete = {
+                    text = '_'
+                },
+                topdelete = {
+                    text = '‾'
+                },
+                changedelete = {
+                    text = '~'
+                }
+            },
+            on_attach = function(bufnr)
+                vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, {
+                    buffer = bufnr,
+                    desc = 'Preview git hunk'
+                })
+
+                -- don't override the built-in and fugitive keymaps
+                local gs = package.loaded.gitsigns
+                vim.keymap.set({ 'n', 'v' }, ']c', function()
+                    if vim.wo.diff then
+                        return ']c'
+                    end
+                    vim.schedule(function()
+                        gs.next_hunk()
+                    end)
+                    return '<Ignore>'
+                end, {
+                    expr = true,
+                    buffer = bufnr,
+                    desc = "Jump to next hunk"
+                })
+                vim.keymap.set({ 'n', 'v' }, '[c', function()
+                    if vim.wo.diff then
+                        return '[c'
+                    end
+                    vim.schedule(function()
+                        gs.prev_hunk()
+                    end)
+                    return '<Ignore>'
+                end, {
+                    expr = true,
+                    buffer = bufnr,
+                    desc = "Jump to previous hunk"
+                })
+            end
+        }
+    },
+    {
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
+        config = function()
+            vim.cmd [[colorscheme gruvbox]]
+        end
+    },
+    {
+        -- Set lualine as statusline
+        'nvim-lualine/lualine.nvim',
+        -- See `:help lualine.txt`
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = 'gruvbox',
+                component_separators = '|',
+                section_separators = ''
             }
         }
     },
-    init = function()
-        -- If you want the formatexpr, here is the place to set it
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end
-}, -- Useful plugin to show you pending keybinds.
-{
-    'folke/which-key.nvim',
-    opts = {}
-}, {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-        -- See `:help gitsigns.txt`
-        signs = {
-            add = {
-                text = '+'
-            },
-            change = {
-                text = '~'
-            },
-            delete = {
-                text = '_'
-            },
-            topdelete = {
-                text = '‾'
-            },
-            changedelete = {
-                text = '~'
-            }
-        },
-        on_attach = function(bufnr)
-            vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, {
-                buffer = bufnr,
-                desc = 'Preview git hunk'
-            })
-
-            -- don't override the built-in and fugitive keymaps
-            local gs = package.loaded.gitsigns
-            vim.keymap.set({'n', 'v'}, ']c', function()
-                if vim.wo.diff then
-                    return ']c'
-                end
-                vim.schedule(function()
-                    gs.next_hunk()
-                end)
-                return '<Ignore>'
-            end, {
-                expr = true,
-                buffer = bufnr,
-                desc = "Jump to next hunk"
-            })
-            vim.keymap.set({'n', 'v'}, '[c', function()
-                if vim.wo.diff then
-                    return '[c'
-                end
-                vim.schedule(function()
-                    gs.prev_hunk()
-                end)
-                return '<Ignore>'
-            end, {
-                expr = true,
-                buffer = bufnr,
-                desc = "Jump to previous hunk"
-            })
-        end
-    }
-}, {
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000,
-    config = function()
-        vim.cmd [[colorscheme gruvbox]]
-    end
-}, {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = 'gruvbox',
-            component_separators = '|',
-            section_separators = ''
-        }
-    }
-}, {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    main = "ibl",
-    opts = {}
-}, -- "gc" to comment visual regions/lines
-{
-    'numToStr/Comment.nvim',
-    opts = {}
-}, -- Fuzzy Finder (files, lsp, etc)
-{
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {'nvim-lua/plenary.nvim', -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-    -- Only load if `make` is available. Make sure you have the system
-    -- requirements installed.
     {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-            return vim.fn.executable 'make' == 1
-        end
-    }}
-}, {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {'nvim-treesitter/nvim-treesitter-textobjects'},
-    build = ':TSUpdate'
-}, -- Copilot
-{
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = "InsertEnter",
-    config = function()
-        require('copilot').setup({
-            suggestion = {
-                enabled = false
-            },
-            panel = {
-                enabled = false
-            }
-        })
-    end
-}, {
-    'zbirenbaum/copilot-cmp',
-    config = function()
-        require('copilot_cmp').setup()
-    end
-}}
+        -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help indent_blankline.txt`
+        main = "ibl",
+        opts = {}
+    }, -- "gc" to comment visual regions/lines
+    {
+        'numToStr/Comment.nvim',
+        opts = {}
+    }, -- Fuzzy Finder (files, lsp, etc)
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim', -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+            -- Only load if `make` is available. Make sure you have the system
+            -- requirements installed.
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                -- NOTE: If you are having trouble with this installation,
+                --       refer to the README for telescope-fzf-native for more instructions.
+                build = 'make',
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end
+            } }
+    },
+    {
+        -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+        build = ':TSUpdate'
+    }
+}
