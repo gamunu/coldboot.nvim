03d2224624d2f0f8aad3aa931fcbe08e56c51ace
Gamunu Balagalla <gamunu@fastcode.io>
2024-08-12 13:04:03 +0530

    feat: add oil to neovim
    Signed-off-by: Gamunu Balagalla <gamunu@fastcode.io>

--- lua/coldboot/base/init.lua
+++ lua/coldboot/base/init.lua
@@ -4,6 +4,39 @@
 return {                                       -- Git related plugins
     'tpope/vim-fugitive', 'tpope/vim-rhubarb', -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',                        -- NOTE: This is where your plugins related to LSP can be installed.
+    {
+        'stevearc/oil.nvim',
+        opts = {
+            -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+            -- Set to false if you still want to use netrw.
+            default_file_explorer = true,
+            -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+            delete_to_trash = true,
+            view_options = {
+                -- Show files and directories that start with "."
+                show_hidden = true,
+                -- This function defines what is considered a "hidden" file
+                is_hidden_file = function(name, bufnr)
+                    return vim.startswith(name, ".")
+                end,
+                -- This function defines what will never be shown, even when `show_hidden` is set
+                is_always_hidden = function(name, bufnr)
+                    return false
+                end,
+                -- Sort file names in a more intuitive order for humans. Is less performant,
+                -- so you may want to set to false if you work with large directories.
+                natural_order = true,
+                sort = {
+                    -- sort order can be "asc" or "desc"
+                    -- see :help oil-columns to see which columns are sortable
+                    { "type", "asc" },
+                    { "name", "asc" },
+                },
+            },
+        },
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
     --  The configuration is done below. Search for lspconfig to find it below.
     {
         -- LSP Configuration & Plugins
@@ -96,7 +129,6 @@ return {                                       -- Git related plugins
     },
     {
         'stevearc/conform.nvim',
-        event = { 'BufWritePre' },
         cmd = 'ConformInfo',
         keys = {
             {
