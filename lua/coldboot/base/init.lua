199f8605a7ea3a416419645ba1406dcd255965a0
Gamunu Balagalla <gamunu@fastcode.io>
2024-08-26 01:19:02 +0530

    feat: add terminal support
    Signed-off-by: Gamunu Balagalla <gamunu@fastcode.io>

--- lua/coldboot/base/init.lua
+++ lua/coldboot/base/init.lua
@@ -290,5 +290,29 @@ return {                 -- Git related plugins
         'nvim-treesitter/nvim-treesitter',
         dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
         build = ':TSUpdate'
-    }
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        keys = {
+            { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>",              desc = "Toggle floating terminal" },
+            { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Toggle horizontal terminal" },
+            { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>",   desc = "Toggle vertical terminal" },
+        },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section in the documentation
+            open_mapping = [[<c-\>]], -- Lazy load on leader key press
+            shading_factor = 2,
+            direction = "float",
+            float_opts = {
+                border = "curved",
+                highlights = {
+                    border = "Normal",
+                    background = "Normal",
+                },
+            },
+        }
+    },
 }
