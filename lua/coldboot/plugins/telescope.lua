de5d8dc322787a2c4f0dab9d052282371b03fe95
Gamunu Balagalla <gamunu@fastcode.io>
2025-10-14 23:09:16 +0530

    feat: replace telescoep with fzf-lua
    Signed-off-by: Gamunu Balagalla <gamunu@fastcode.io>

--- lua/coldboot/plugins/telescope.lua
+++ lua/coldboot/plugins/telescope.lua
@@ -1,53 +1,93 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    cmd = 'Telescope',
+    'ibhagwan/fzf-lua',
+    cmd = 'FzfLua',
     keys = {
-      { '<leader>?', '<cmd>Telescope oldfiles<cr>', desc = '[?] Find recently opened files' },
-      { '<leader><space>', '<cmd>Telescope buffers<cr>', desc = '[ ] Find existing buffers' },
+      {
+        '<leader>?',
+        function()
+          require('fzf-lua').oldfiles()
+        end,
+        desc = '[?] Find recently opened files',
+      },
+      {
+        '<leader><space>',
+        function()
+          require('fzf-lua').buffers()
+        end,
+        desc = '[ ] Find existing buffers',
+      },
       {
         '<leader>/',
         function()
-          require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-          })
+          require('fzf-lua').blines()
         end,
         desc = '[/] Fuzzily search in current buffer',
       },
-      { '<leader>gf', '<cmd>Telescope git_files<cr>', desc = 'Search [G]it [F]iles' },
-      { '<leader>sf', '<cmd>Telescope find_files<cr>', desc = '[S]earch [F]iles' },
-      { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = '[S]earch [H]elp' },
-      { '<leader>sw', '<cmd>Telescope grep_string<cr>', desc = '[S]earch current [W]ord' },
-      { '<leader>sg', '<cmd>Telescope live_grep<cr>', desc = '[S]earch by [G]rep' },
-      { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = '[S]earch [D]iagnostics' },
-      { '<leader>sr', '<cmd>Telescope resume<cr>', desc = '[S]earch [R]esume' },
-    },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
       {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
+        '<leader>gf',
+        function()
+          require('fzf-lua').git_files()
         end,
+        desc = 'Search [G]it [F]iles',
+      },
+      {
+        '<leader>sf',
+        function()
+          require('fzf-lua').files()
+        end,
+        desc = '[S]earch [F]iles',
+      },
+      {
+        '<leader>sh',
+        function()
+          require('fzf-lua').help_tags()
+        end,
+        desc = '[S]earch [H]elp',
+      },
+      {
+        '<leader>sw',
+        function()
+          require('fzf-lua').grep_cword()
+        end,
+        desc = '[S]earch current [W]ord',
+      },
+      {
+        '<leader>sg',
+        function()
+          require('fzf-lua').live_grep()
+        end,
+        desc = '[S]earch by [G]rep',
+      },
+      {
+        '<leader>sd',
+        function()
+          require('fzf-lua').diagnostics_workspace()
+        end,
+        desc = '[S]earch [D]iagnostics',
+      },
+      {
+        '<leader>sr',
+        function()
+          require('fzf-lua').resume()
+        end,
+        desc = '[S]earch [R]esume',
       },
     },
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
     config = function()
-      require('telescope').setup {
-        defaults = {
-          mappings = {
-            i = {
-              ['<C-u>'] = false,
-              ['<C-d>'] = false,
-            },
-          },
+      local fzf = require 'fzf-lua'
+
+      fzf.setup {
+        files = {
+          fd_opts = [[--color=never --type f --hidden --follow --exclude .git]],
+        },
+        grep = {
+          rg_opts = [[--column --line-number --no-heading --color=never --smart-case --hidden --glob !.git/]],
         },
       }
-
-      -- Enable telescope fzf native, if installed
-      pcall(require('telescope').load_extension, 'fzf')
     end,
   },
 }
