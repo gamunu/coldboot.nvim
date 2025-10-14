5e2438fa2f8904b55a5cda0b1817518e2fd44e48
Gamunu Balagalla <gamunu@fastcode.io>
2025-10-14 23:15:20 +0530

    feat: replace telescoep with fzf-lua (#12)
    Signed-off-by: Gamunu Balagalla <gamunu@fastcode.io>

--- lua/coldboot/plugins/lsp.lua
+++ lua/coldboot/plugins/lsp.lua
@@ -39,14 +39,16 @@ return {
             vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
           end
 
+          local fzf_lua = require 'fzf-lua'
+
           nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
           nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-          nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          nmap('gr', fzf_lua.lsp_references, '[G]oto [R]eferences')
+          nmap('gI', fzf_lua.lsp_implementations, '[G]oto [I]mplementation')
           nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-          nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          nmap('<leader>ds', fzf_lua.lsp_document_symbols, '[D]ocument [S]ymbols')
+          nmap('<leader>ws', fzf_lua.lsp_live_workspace_symbols, '[W]orkspace [S]ymbols')
           nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
           nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
           nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
@@ -62,14 +64,16 @@ return {
           vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
         end
 
+        local fzf_lua = require 'fzf-lua'
+
         nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
         nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
         nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-        nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        nmap('gr', fzf_lua.lsp_references, '[G]oto [R]eferences')
+        nmap('gI', fzf_lua.lsp_implementations, '[G]oto [I]mplementation')
         nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-        nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        nmap('<leader>ds', fzf_lua.lsp_document_symbols, '[D]ocument [S]ymbols')
+        nmap('<leader>ws', fzf_lua.lsp_live_workspace_symbols, '[W]orkspace [S]ymbols')
         nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
         nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
         nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
